842cd91bb3b0532e93c7a2a9b9f18e9f057702a3
soufcode <soufiane.ait@outlook.fr>
2022-12-24 21:27:49 +0100

    db conn update

--- src/utils.ts
+++ src/utils.ts
@@ -6,7 +6,7 @@ import { Category } from "./entities/Category";
 
 const datasource = new DataSource({
   type: "mysql",
-  host: "localhost",
+  host: "127.0.0.1",
   port: 3306,
   username: "etracker",
   password: "3rxt@95VF5ht",
