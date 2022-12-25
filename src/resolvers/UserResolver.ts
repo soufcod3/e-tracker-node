a02ef54fba2e286cff6dea22c38de4983e774cfe
soufcode <soufiane.ait@outlook.fr>
2022-12-25 21:33:42 +0100

    remove argon2

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,18 +1,10 @@
-import { Resolver, Mutation, Arg, Query } from "type-graphql";
-import { User, UserInput } from "../entities/User";
+import { Resolver, Arg, Query } from "type-graphql";
+import { User } from "../entities/User";
 import datasource from "../utils";
-import { hash } from "argon2";
+// import { hash } from "argon2";
 
 @Resolver()
 export class UsersResolver {
-  @Mutation(() => User)
-  async createUser(
-    @Arg("data", () => UserInput) data: UserInput
-  ): Promise<User> {
-    data.password = await hash(data.password);
-    return await datasource.getRepository(User).save(data);
-  }
-
   @Query(() => [User])
   async users(): Promise<User[]> {
     return await datasource.getRepository(User).find({});
