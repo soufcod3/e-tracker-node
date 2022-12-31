15e43c9da748bde944318c16ecb116237877ee84
soufcode <soufiane.ait@outlook.fr>
2022-12-31 21:41:25 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -14,78 +14,74 @@ import http from 'http';
 import https from 'https';
 import fs from 'fs';
 
-async function bootstrap(): Promise<void> {
+async function bootstrap(): Promise<any> {
+  // ... Building schema here
+  const schema = await buildSchema({
+    resolvers: [
+      UsersResolver,
+      BudgetsResolver,
+      ExpensesResolver,
+      CategoriesResolver
+    ],
+  });
+
+  const configurations = {
+    // Note: You may need sudo to run on port 443
+    production: { ssl: true, port: 443, hostname: 'example.com' },
+    development: { ssl: false, port: 5000, hostname: 'localhost' },
+  };
+
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  const environment = process.env.NODE_ENV ?? 'development';
+  const config = configurations[environment];
+
+  // Create the GraphQL server
+  const server = new ApolloServer({
+    schema,
+    cache: 'bounded'
+  });
+  await server.start();
+
+  const app = express();
+  server.applyMiddleware({ app })
+  // our express server is mounted at /
+  // app.use('/', cors<cors.CorsRequest>(), bodyParser.json(), expressMiddleware(server));
+  // app.use('/', cors<cors.CorsRequest>())
+  let httpServer;
+
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (config.ssl) {
+    // Assumes certificates are in a .ssl folder off of the package root.
+    // Make sure these files are secured.
+    httpServer = https.createServer(
+      {
+        key: fs.readFileSync(`/etc/letsencrypt/live/e-tracker-server.soufcode.fr-0001/fullchain.pem`),
+        cert: fs.readFileSync(`/etc/letsencrypt/live/e-tracker-server.soufcode.fr-0001/privkey.pem`),
+      },
+      app,
+    );
+  } else {
+    httpServer = http.createServer(app);
+  }
+
   try {
-    // ... Building schema here
-    const schema = await buildSchema({
-      resolvers: [
-        UsersResolver,
-        BudgetsResolver,
-        ExpensesResolver,
-        CategoriesResolver
-      ],
-    });
+    await httpServer.listen({ port: config.port });
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    console.log(`🚀 Server ready on port ${config.port}`);
+  } catch (err) {
+    console.log("🚧A GraphQL server error occured");
+    console.error(err);
+  }
 
-    const configurations = {
-      // Note: You may need sudo to run on port 443
-      production: { ssl: true, port: 443, hostname: 'example.com' },
-      development: { ssl: false, port: 5000, hostname: 'localhost' },
-    };
+  // await new Promise<void>((resolve) => httpServer.listen({ port: config.port }, resolve)).catch(err => console.log('Promise Error :', err));
 
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    const environment = process.env.NODE_ENV ?? 'development';
-    const config = configurations[environment];
-
-    // Create the GraphQL server
-    const server = new ApolloServer({
-      schema,
-      cache: 'bounded'
-    });
-    await server.start();
-
-    const app = express();
-    server.applyMiddleware({app})
-    // our express server is mounted at /
-    // app.use('/', cors<cors.CorsRequest>(), bodyParser.json(), expressMiddleware(server));
-    app.use('/', cors<cors.CorsRequest>())
-    let httpServer;
-
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (config.ssl) {
-      // Assumes certificates are in a .ssl folder off of the package root.
-      // Make sure these files are secured.
-      httpServer = https.createServer(
-        {
-          key: fs.readFileSync(`/etc/letsencrypt/live/e-tracker-server.soufcode.fr-0001/fullchain.pem`),
-          cert: fs.readFileSync(`/etc/letsencrypt/live/e-tracker-server.soufcode.fr-0001/privkey.pem`),
-        },
-        app,
-      );
-    } else {
-      httpServer = http.createServer(app);
-    }
-
-    try {
-      await httpServer.listen({port: config.port});
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      console.log(`🚀 Server ready on port ${config.port}`);
-    } catch (err) {
-      console.log("🚧A GraphQL server error occured");
-      console.error(err);
-    }
-
-    // await new Promise<void>((resolve) => httpServer.listen({ port: config.port }, resolve)).catch(err => console.log('Promise Error :', err));
-
-    try {
-      await datasource.initialize();
-      console.log("Server started!");
-      console.log(`On the road to deployment : env=${environment}`)
-    } catch (err) {
-      console.log("An error occured");
-      console.error(err);
-    }
-  } catch (error) {
-    console.log(error)
+  try {
+    await datasource.initialize();
+    console.log("Server started!");
+    console.log(`On the road to deployment : env=${environment}`)
+  } catch (err) {
+    console.log("An error occured");
+    console.error(err);
   }
 }
 
