e732f5aa48f5ed59966081eba0b08a5071f7f5b2
soufcode <soufiane.ait@outlook.fr>
2022-12-31 19:16:12 +0100

    installing express

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import "reflect-metadata";
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "@apollo/server";
 import datasource from "./utils";
 import { buildSchema } from "type-graphql";
 import { UsersResolver } from "./resolvers/UserResolver";
 import { BudgetsResolver } from "./resolvers/BudgetResolver";
 import { ExpensesResolver } from "./resolvers/ExpenseResolver";
 import { CategoriesResolver } from "./resolvers/CategoryResolver";
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { expressMiddleware } from '@apollo/server/express4';
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
 
 console.log('On the road to deployment !')
 
@@ -20,23 +27,50 @@ async function bootstrap(): Promise<void> {
     ],
   });
 
-  console.log('env', process.env.NODE_ENV)
+  const configurations = {
+    // Note: You may need sudo to run on port 443
+    production: { ssl: true, port: 443, hostname: 'example.com' },
+    development: { ssl: false, port: 4000, hostname: 'localhost' },
+  };
+
+  const environment = process.env.NODE_ENV || 'development';
+  const config = configurations[environment];
 
   // Create the GraphQL server
   const server = new ApolloServer({
-    schema,
-    cors: true,
+    schema
   });
+  await server.start();
 
-  // Start the server
-  const { url } = await server.listen({port: process.env.PORT || 5000});
-  console.log(`Server is running, GraphQL Playground available at ${url}`);
+  const app = express();
+  // our express server is mounted at /
+  app.use('/', cors<cors.CorsRequest>(), bodyParser.json(), expressMiddleware(server));
+
+  let httpServer;
+
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (config.ssl) {
+    // Assumes certificates are in a .ssl folder off of the package root.
+    // Make sure these files are secured.
+    httpServer = https.createServer(
+      {
+        key: fs.readFileSync(`./ssl/${environment}/server.key`),
+        cert: fs.readFileSync(`./ssl/${environment}/server.crt`),
+      },
+      app,
+    );
+  } else {
+    httpServer = http.createServer(app);
+    console.log(httpServer)
+  }
+
+  await new Promise<void>((resolve) => httpServer.listen({ port: 5000 }, resolve));
 
   try {
     await datasource.initialize();
     console.log("Server started!");
   } catch (err) {
-    console.log("Damn, an error occured");
+    console.log("An error occured");
     console.error(err);
   }
 }
