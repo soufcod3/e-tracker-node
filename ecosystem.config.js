bdff8b6b35fc7ec2a7b75e8e9c6ab7390a386fc6
soufcode <soufiane.ait@outlook.fr>
2023-01-01 15:39:03 +0100

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       repo : 'git@github.com:soufcod3/e-tracker-node.git',
       path : '/var/www/e-tracker-node',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && tsc && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'npm install && tsc && pm2 reload ecosystem.config.js && pm2 ls',
       'pre-setup': ''
     }
   }
