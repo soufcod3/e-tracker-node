92c8a7b64e5a63bb4db9ad577d90b3e9f606e87c
soufcode <soufiane.ait@outlook.fr>
2023-01-01 15:01:38 +0100

    pm2 setup

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    script: 'src/index.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '89.117.37.18',
+      ref  : 'origin/main',
+      repo : 'git@github.com:soufcod3/e-tracker-node.git',
+      path : '/var/www/e-tracker-node',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && tsc && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
